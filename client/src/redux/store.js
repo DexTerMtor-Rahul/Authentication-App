a1db2f0b613ef419b837c413079aa469620de42e
DexTerMtor <dextermtor.rahulraj@gmail.com>
2024-01-31 22:48:11 +0530

    add redux persist to store and add persistGate to app component to persist state on refresh

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+// Combining all reducers into a single root reducer
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+// Configuration for redux-persist, which allows the Redux state to be stored in local storage
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+// Creating a persisted reducer using the root reducer and the persist configuration
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configuring the Redux store
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
 
   //wehen we use middleware we need to disable serializableCheck to avoid error in console.
   middleware: (getDefaultMiddleware) =>
@@ -10,6 +28,9 @@ export const store = configureStore({
       serializableCheck: false,
     }),
 });
+
+// Creating a persistor, which is used to persist and rehydrate the Redux store
+export const persistor = persistStore(store);
 
 // store.js is the place where we create our Redux store.
 // We use configureStore() to create the store.
