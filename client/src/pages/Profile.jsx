7af047eec81f22d02415f5b165fcc9b31d670e1e
DexTerMtor <dextermtor.rahulraj@gmail.com>
2024-02-02 11:12:09 +0530

    create upload image functionlity (firebase bug to be handle)

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,50 @@
 import { useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [image, setImage] = useState(undefined);
+  const [imagePercent, setImagePercent] = useState(0);
+  const [imageError, setImageError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  useEffect(() => {
+    if (image) {
+      handleFileUpload();
+    }
+  }, [image]);
+
+  const handleFileUpload = async (image) => {
+    const storage = getStorage(app);
+    const fileName = image ? new Date().getTime() + image.name : "";
+    const storageRef = ref(storage, "images/" + fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImagePercent(Math.round(progress));
+      },
+      // eslint-disable-next-line no-unused-vars
+      (error) => {
+        setImageError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, profilePicture: downloadURL })
+        );
+      }
+    );
+  };
 
   return (
     <div className="bg-indigo-50 min-h-screen">
@@ -9,11 +52,34 @@ export default function Profile() {
         <div className="mt-10 bg-white w-full rounded-lg shadow-md px-8 py-8 ">
           <h1 className="mb-8 text-3xl font-semibold text-center">Profile</h1>
           <div className="mb-10 flex flex-col items-center justify-center cursor-pointer">
+            <input
+              type="file"
+              ref={fileRef}
+              hidden
+              accept="image/*"
+              onChange={(e) => setImage(e.target.files[0])}
+            />
             <img
-              src={currentUser.profilePicture}
+              src={formData.profilePicture || currentUser.profilePicture}
               alt="profile"
               className="h-100 w-100 rounded-full object-cover"
+              onClick={() => fileRef.current.click()}
             />
+            <p className="mt-2 text-sm self-center">
+              {imageError ? (
+                <span className="text-red-700">
+                  Error uploading image (file size must be less than 2 MB)
+                </span>
+              ) : imagePercent > 0 && imagePercent < 100 ? (
+                <span className="text-slate-700">{`Uploading: ${imagePercent} %`}</span>
+              ) : imagePercent === 100 ? (
+                <span className="text-green-700">
+                  Image uploaded successfully
+                </span>
+              ) : (
+                ""
+              )}
+            </p>
           </div>
           <form>
             <input
